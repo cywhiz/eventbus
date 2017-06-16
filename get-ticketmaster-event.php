6701cb047d0e7874824733e5e88491c2a60fd861
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 14:06:03 +0100

    placeholder

---
+++ get-ticketmaster-event.php
@@ -0,0 +1,11 @@
+<?php
+function getTicketmasterEvent($event_id) {
+	//TODO CALL TICKETMASTER
+	
+	//Output fields
+	return [
+        "name" => "U2 yadda yadda",
+        "latitude" => "/51.53198",
+        "longitude" => "-0.10724"
+    ];
+}
