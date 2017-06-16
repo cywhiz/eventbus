42185fdda24fc72bd724147d592f4a47419add30
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 14:47:42 +0100

    map!

--- event.php
+++ event.php
@@ -7,24 +7,34 @@ $event_id = $_GET['id'];
 $event = getTicketmasterEvent($event_id);
 ?>
 <html>
-<body>
 <head>
   <title>event</title>
-  <script language="javascript" src="./main.js">
-  <script language="javascript" src="./ticketmaster.js">
-  <script language="javascript" src="./tfl.js">
+  <script src="./main.js" /></script>
+ 
+  <!-- leaflet map includes -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+  <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
   
   <script language="javascript">
-  event = "<?php=$event ?>";
+    values from php -> javascript
+    var eventName = "<?php echo $event['name']; ?>";
+    var eventLat = "<?php echo $event['lat']; ?>";
+    var eventLon = "<?php echo $event['lon']; ?>";
+  
   </script>
 
-  <link rel="stylesheet" type=text/css href="main.css">
+  <link rel="stylesheet" type=text/css href="./main.css">
 
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body onload="init();">
-  <h1>Event</h1>
-  <div id='map'></div>
+   <h1>Event</h1>
+  
+   <div id="map" style="height:400px; width:400px;">
+   map
+   </div>
 </body>
+
 </html>
