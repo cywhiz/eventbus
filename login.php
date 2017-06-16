cc4c11cfc4f3fc44daf9a4832b5ca8a0c786f51c
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 14:12:57 +0100

    new login placeholder

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<head>
+  <title>login</title>
+  
+  
+  <link rel="stylesheet" type=text/css href="main.css">
+
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <h1>Login</h1>
+  
+  TODO
+</body>
+</html>
