f9f6027b763a14b0962aafc579ad63a030281c9e
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 14:48:36 +0100

    map!

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function init() {
-         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-            maxZoom: 18
-         }).addTo(map);
-         map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
 
-         var london = new L.LatLng(51.505, -0.09); // geographical point (longitude and latitude)
-         map.setView(london, 13);
+function init() {
+  var map = L.map('map');
+ 	 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+     maxZoom: 18
+  }).addTo(map);
+  map.attributionControl.setPrefix(''); // Don't show the 'Powered by Leaflet' text.
+  
+  var london = new L.LatLng(51.505, -0.09); // geographical point (longitude and latitude)
+  map.setView(london, 13);
 }
