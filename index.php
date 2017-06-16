e23d9eb4eac8a461fcba6ff6e2449da10ba3d4d1
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 13:50:03 +0100

    index ideas

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php
-if (!isset($_GET['id'])) die('missing id');
-$event_id = $_GET['id'];
-$event = { };
+$text = !isset($_GET['text']) ? $_GET['text'] : "";
+
+//TODO show text box (with or without text)
+
+//TODO call ticketmaster
+
+//TODO show list with links to event.php?id=efewfeweqf
 ?>
-<html>
-<body>
-<head>
-  <title>event</title>
-  <script language="javascript" src="./main.js">
-  <script language="javascript" src="./ticketmaster.js">
-  <script language="javascript" src="./tfl.js">
-  
-  <script language="javascript">
-  event = "<?php=$event ?>";
-  </script>
-
-  <link rel="stylesheet" type=text/css href="main.css">
-
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body onload="init();">
-  <h1>Event</h1>
-  <div id='map'></div>
-</body>
-</html>
