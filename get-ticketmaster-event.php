7cda146040d62868ee7a2b75b00b7d7c3a8ea64a
Harry Wood <github@onlineanimals.co.uk>
2017-06-16 14:06:53 +0100

    fix formatting

--- get-ticketmaster-event.php
+++ get-ticketmaster-event.php
@@ -1,11 +1,11 @@
 <?php
 function getTicketmasterEvent($event_id) {
-	//TODO CALL TICKETMASTER
+    //TODO CALL TICKETMASTER
 	
-	//Output fields
-	return [
+    //Output fields
+    return [
         "name" => "U2 yadda yadda",
-        "latitude" => "/51.53198",
+        "latitude" => "51.53198",
         "longitude" => "-0.10724"
     ];
 }
